739e5ed16a21b8d18d09ebab88a578383ba8a745
joelcena <joelcena.47@gmail.com>
2022-11-21 18:54:47 -0800

    ft-icons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./components/header/Header";
 function App() {
   return (
     <div className='app'>
-      <h1>Lets build the facebook clone!</h1>
       <Header />
     </div>
   );
