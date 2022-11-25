e6cd421d2fa2eb1bbe8d5bc2dfa2ddb37101141b
joelcena <joelcena.47@gmail.com>
2022-11-25 13:36:47 -0800

    ft-loginhandler

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Feed from "./components/feed/Feed";
 import Header from "./components/header/Header";
 import Sidebar from "./components/header/sidebar/Sidebar";
 import Widgets from "./components/widgets/Widgets";
+import Login from "./login/Login";
 
 function App() {
+  const user = null;
+
   return (
     <div className='app'>
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
