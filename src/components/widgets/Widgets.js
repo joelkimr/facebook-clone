16f3c32316e77ab94a0a36ff2037869090e41ed5
joelcena <joelcena.47@gmail.com>
2022-12-07 14:34:41 -0800

    ft-posters

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -7,7 +7,7 @@ function Widgets() {
       <iframe
         src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
         width='340'
-        height='100%'
+        height='140%'
         style={{ border: "none", overflow: "hidden" }}
         scrolling='no'
         frameBorder='0'
